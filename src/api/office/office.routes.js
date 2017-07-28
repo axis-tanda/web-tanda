6d74a3972e8fb666a3f185c95629d492cf1c6efb
septa97 <jancharles.adona@gmail.com>
2017-07-28 22:23:54 +0800

    Removed office endpoint

--- src/api/office/office.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as controller from './user.controller';
-
-const router = express.Router();
-
-router.get('/', controller.getAll);
-router.get('/:id', controller.getUser);
-
-export default router;
