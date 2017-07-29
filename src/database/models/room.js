3b19a9a6d8d7b49aaf8f1bd37fbdd8e1970cd068
Rain Tomista <rftomista@gmail.com>
2017-07-29 13:38:36 +0800

    Room schema

--- src/database/models/room.js
+++ src/database/models/room.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const roomSchema = mongoose.Schema({
   room_id: { type: Number},
   room_name: { type: String},
-  data: { type: [Object]},
+  data: { type: Array},
   isVacant: {type: Boolean}
 });
 
